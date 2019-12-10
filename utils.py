7441302974c011542adc7af58a249622b3911de8
kikokushijo <zoo30215@gmail.com>
2019-12-10 13:55:42 +0800

    Refactoring Repeated Script; Add strong tenacity erorr handling

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import configparser
 import datetime
 import itertools
 import os
@@ -15,6 +16,9 @@ from selenium import webdriver
 
 ArticleMeta = namedtuple('ArticleMeta', ['url', 'date', 'time', 'category', 'title', 'content'])
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
 class NewsCrawler(object):
     
     def __init__(self, output_dir, total_days, start_date=datetime.date.today()):
@@ -30,7 +34,7 @@ class NewsCrawler(object):
                           "Chrome/69.0.3497.92 Safari/537.36"
         }
         self.scroll_pause_time = 0.3
-        self.driver = webdriver.PhantomJS(executable_path='../phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+        self.driver = webdriver.PhantomJS(executable_path=config['Dependency']['PhantomJSPath'])
         self.output_dir = output_dir
         self.total_days = total_days
         self.start_date = start_date
@@ -69,3 +73,77 @@ class NewsCrawler(object):
         for _ in range(self.total_days):
             yield str(date)
             date = date - datetime.timedelta(days=1)
+    
+    @retry(stop=stop_after_attempt(0),
+           retry_error_callback=lambda x: None)
+    def parse_category(self, newspage):
+        raise NotImplementedError
+    
+    @retry(stop=stop_after_attempt(0),
+           retry_error_callback=lambda x: None)
+    def parse_title(self, newspage):
+        raise NotImplementedError
+    
+    @retry(stop=stop_after_attempt(0),
+           retry_error_callback=lambda x: None)
+    def parse_article(self, newspage):
+        raise NotImplementedError
+    
+    @retry(stop=stop_after_attempt(0),
+           retry_error_callback=lambda x: (None, None))
+    def parse_date_time(self, newspage):
+        raise NotImplementedError
+
+    def is_valid_newspage(self, newspage):
+        raise NotImplementedError
+
+    def saved_filename(self, url):
+        raise NotImplementedError
+
+    def parse_page_attribute(self, newslink, newspage):
+
+        date_str, time_str = self.parse_date_time(newspage)
+        category = self.parse_category(newspage)
+        title = self.parse_title(newspage)
+        text = self.parse_article(newspage)
+
+        if text is None or date_str is None or time_str is None:
+            return None
+
+        article_meta = ArticleMeta(
+            url=newslink,
+            date=date_str,
+            time=time_str,
+            category=category,
+            title=title,
+            content=text
+        )
+
+        return article_meta
+    
+    def get_page_attribute_from_link(self, newslink):
+        if newslink in self.newslinks:
+            print('Duplicated URL:', newslink)
+            return None
+        else:
+            self.newslinks.add(newslink)
+
+        page = self.get_bsObj(newslink)
+        if not self.is_valid_newspage(page):
+            print('Invalid or Redirected Page:', newslink)
+            return None
+        
+        article = self.parse_page_attribute(newslink, page)          
+        if article is None:
+            print('Invalid or Redirected Page:', newslink)
+            return None
+        
+        print('Successful Scraping URL:', newslink)
+        return article
+    
+    def save_article_meta(self, article):
+        output_dir_with_date = os.path.join(self.output_dir, article.date)
+        os.makedirs(output_dir_with_date ,exist_ok=True)
+        filename = os.path.join(output_dir_with_date, self.saved_filename(article.url))
+        with open(filename, 'w+', encoding='utf-8') as f:
+            json.dump(article._asdict(), f, ensure_ascii=False, indent=4)
